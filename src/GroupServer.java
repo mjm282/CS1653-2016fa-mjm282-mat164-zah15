43984ab4f6eb3aa1fe95e25379a9a128729ea10d
zhankinson <zah15@pitt.edu>
2016-09-29 13:04:26 -0400

    Group Client implementation started

--- src/GroupServer.java
+++ src/GroupServer.java
@@ -1,6 +1,6 @@
 /* Group server. Server loads the users from UserList.bin.
  * If user list does not exists, it creates a new list and makes the user the server administrator.
- * On exit, the server saves the user list to file. 
+ * On exit, the server saves the user list to file.
  */
 
 /*
@@ -15,31 +15,35 @@ import java.io.*;
 import java.util.*;
 
 
-public class GroupServer extends Server {
+public class GroupServer extends Server
+{
 
 	public static final int SERVER_PORT = 8765;
 	public UserList userList;
-    
+	public GroupList groupList;
+
 	public GroupServer() {
 		super(SERVER_PORT, "ALPHA");
 	}
-	
+
 	public GroupServer(int _port) {
 		super(_port, "ALPHA");
 	}
-	
+
 	public void start() {
 		// Overwrote server.start() because if no user file exists, initial admin account needs to be created
-		
+
 		String userFile = "UserList.bin";
+		String groupFile = "GroupList.bin";
+
 		Scanner console = new Scanner(System.in);
 		ObjectInputStream userStream;
 		ObjectInputStream groupStream;
-		
+
 		//This runs a thread that saves the lists on program exit
 		Runtime runtime = Runtime.getRuntime();
 		runtime.addShutdownHook(new ShutDownListener(this));
-		
+
 		//Open user file to get user list
 		try
 		{
@@ -53,10 +57,16 @@ public class GroupServer extends Server {
 			System.out.println("No users currently exist. Your account will be the administrator.");
 			System.out.print("Enter your username: ");
 			String username = console.next();
-			
+
 			//Create a new list, add current user to the ADMIN group. They now own the ADMIN group.
 			userList = new UserList();
+			groupList = new GroupList();
 			userList.addUser(username);
+			//CHECK GROUPLIST IMPLEMENTATION
+			/*
+			groupList.addGroup("ADMIN");
+			groupList.addUser(username);
+			*/
 			userList.addGroup(username, "ADMIN");
 			userList.addOwnership(username, "ADMIN");
 		}
@@ -70,21 +80,58 @@ public class GroupServer extends Server {
 			System.out.println("Error reading from UserList file");
 			System.exit(-1);
 		}
-		
+
+		//Open group file to get group list
+		try
+		{
+			fis = new FileInputStream(groupFile);
+			groupStream = new ObjectInputStream(fis);
+			groupList = (GroupList)groupstream.readObject();
+			//runtime.gc();
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("GroupList File Does Not Exist. Creating GroupList");
+			System.out.println("No groups currently exist. Group \"ADMIN\" will be created.");
+			System.out.println("Your account will be the administrator.");
+			System.out.print("Enter your username: ");
+			String username = console.next();
+
+			//Create a new group list, add current user to the ADMIN group. They now own the ADMIN group.
+			groupList = new GroupList();
+			//CHECK GROUPLIST IMPLEMENTATION
+			/*
+			groupList.addGroup("ADMIN");
+			groupList.addUser(username);
+			*/
+			userList.addGroup(username, "ADMIN");
+			userList.addOwnership(username, "ADMIN");
+		}
+		catch(IOException e)
+		{
+			System.out.prinln("Error reading from GroupList file");
+			System.exit(-1);
+		}
+		catch(ClassNotFoundException e)
+		{
+			System.out.println("Error reading from GroupList file");
+			System.exit(-1);
+		}
+
 		//Autosave Daemon. Saves lists every 5 minutes
 		AutoSave aSave = new AutoSave(this);
 		aSave.setDaemon(true);
 		aSave.start();
-		
+
 		//This block listens for connections and creates threads on new connections
 		try
 		{
-			
+
 			final ServerSocket serverSock = new ServerSocket(port);
-			
+
 			Socket sock = null;
 			GroupThread thread = null;
-			
+
 			while(true)
 			{
 				sock = serverSock.accept();
@@ -99,18 +146,18 @@ public class GroupServer extends Server {
 		}
 
 	}
-	
+
 }
 
 //This thread saves the user list
 class ShutDownListener extends Thread
 {
 	public GroupServer my_gs;
-	
+
 	public ShutDownListener (GroupServer _gs) {
 		my_gs = _gs;
 	}
-	
+
 	public void run()
 	{
 		System.out.println("Shutting down server");
@@ -125,17 +172,27 @@ class ShutDownListener extends Thread
 			System.err.println("Error: " + e.getMessage());
 			e.printStackTrace(System.err);
 		}
+		try
+		{
+			outStream = new ObjectOutputStream(new FileOutputStream("GroupList.bin"));
+			outStream.writeObject(my_gs.groupList);
+		}
+		catch(Exception e)
+		{
+			System.err.println("Error: " + e.getMessage());
+			e.printStackTrace(System.err);
+		}
 	}
 }
 
 class AutoSave extends Thread
 {
 	public GroupServer my_gs;
-	
+
 	public AutoSave (GroupServer _gs) {
 		my_gs = _gs;
 	}
-	
+
 	public void run()
 	{
 		do
@@ -149,6 +206,16 @@ class AutoSave extends Thread
 				{
 					outStream = new ObjectOutputStream(new FileOutputStream("UserList.bin"));
 					outStream.writeObject(my_gs.userList);
+				}
+				catch(Exception e)
+				{
+					System.err.println("Error: " + e.getMessage());
+					e.printStackTrace(System.err);
+				}
+				try
+				{
+					outStream = new ObjectOutputStream(new FileOutputStream("GroupList.bin"));
+					outStream.writeObject(my_gs.groupList);
 				}
 				catch(Exception e)
 				{
--- src/UserToken.java
+++ src/UserToken.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 /**
  * A simple interface to the token data structure that will be
- * returned by a group server.  
+ * returned by a group server.
  *
  * You will need to develop a class that implements this interface so
  * that your code can interface with the tokens created by your group
@@ -23,8 +23,6 @@ public interface UserToken
      *
      */
     public String getIssuer();
-
-
     /**
      * This method should return a string indicating the name of the
      * subject of the token.  For instance, if "Alice" requests a
@@ -35,8 +33,6 @@ public interface UserToken
      *
      */
     public String getSubject();
-
-
     /**
      * This method extracts the list of groups that the owner of this
      * token has access to.  If "Alice" is a member of the groups "G1"
